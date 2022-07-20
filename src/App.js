76f060de88053fbdb9f83817627b9db3a80f5b0b
Sajina Shakya <sajina2shakya@gmail.com>
2022-07-20 12:50:37 +0545

    Verify email

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import Profile from './Profile'
+import Register from './register/Register'
+import VerifyEmail from './verifyEmail/VerifyEmail';
+// import Login from './Login'
+import { useState, useEffect } from 'react'
+import { AuthProvider } from './AuthContext.js'
+import { auth } from './firebase'
+import { onAuthStateChanged } from 'firebase/auth'
+import PrivateRoute from './PrivateRoute'
+import { Navigate } from 'react-router-dom'
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState(null)
+  const [timeActive, setTimeActive] = useState(false)
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AuthProvider value={{ currentUser, timeActive, setTimeActive }}>
+        <Routes>
+          <Route exact path='/' element={
+            <PrivateRoute>
+              {/* <Profile/> */}
+            </PrivateRoute>
+          } />
+          <Route path="/login"
+          // element={
+          //   !currentUser?.emailVerified 
+          //   ? <Login/>
+          //   : <Navigate to='/' replace/>
+          // } 
+          />
+          <Route path="/register"
+            element={
+              !currentUser?.emailVerified
+                ? <Register />
+                : <Navigate to='/' replace />
+            } />
+          <Route path='/verify-email' element={<VerifyEmail />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
   );
 }
 
